26bd2843dbbee50bb20bb14d668b68311bebed11
Sanjay <sanjayshetty2015@gmail.com>
2018-04-04 22:25:11 +0530

    matrix

---
+++ p3.py
@@ -0,0 +1,44 @@
+def main():
+    print("Enter the number of teams")
+    n=5
+    print("enter number of games won by each team")
+    W=[0,0,0,0,0]
+    print("numbre of mathes remaining btw R and W")
+    R=[[0,4,4,4,4],[4,0,4,4,4],[4,4,0,4,4],[4,4,4,0,4],[4,4,4,4,0]]
+    #number of game nodes is number matches in the upper triangular matrix 
+
+    print("enter the team number you want to test ranges from 0 to n-1")
+    t=2
+    remainingfort=0
+    for i in range(n):
+        remainingfort+=R[t][i]
+
+    #creating the graph matrix
+    total=2+((n-1)*n)//2
+    capacitymatrix=[[0 for i in range(total+1)]for i in range(total+1)]
+    #index
+    source=0
+    destination=total
+    gamesstart=1
+    gamesend=((n-2)*(n-1))//2
+    teamstart=gamesend+1
+    teamend=total-1
+    #print(gamesend-gamesstart," ",gamesstart," ",gamesend," ",total)
+    #matrix init
+    #count goes from gamestart to gamesend
+    count=gamesstart 
+    for i in range(n):
+        for j in range((i+1),n):
+            if (i!=t) and (j!=t):
+                capacitymatrix[0][count]=R[i][j]
+                capacitymatrix[count][teamstart+i]=10000
+                capacitymatrix[count][teamstart+j]=10000
+                capacitymatrix[teamstart+i][total]=W[t]+remainingfort-W[i]
+                capacitymatrix[teamstart+j][total]=W[t]+remainingfort-W[j]
+                count=count+1
+    for i in range(total+1):
+        for j in range(total+1):
+            print(capacitymatrix[i][j]," ",end="")
+        print()
+if __name__ == '__main__':
+    main()
